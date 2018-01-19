654c818d20fbca6622e82b3fbcf90d63216053ec
shaycormac <574583006@qq.com>
2018-01-19 09:09:48 +0800

    Merge branch 'master' of https://github.com/shaycormac/ShayGoldMantisStudy
    # Conflicts:
    #	.gitignore
    #	app/src/main/java/com/goldmantis/shaystudy/MainActivity.java
    #	settings.gradle
    
    git 更新？？

--- app/src/main/java/com/goldmantis/shaystudy/MainActivity.java
+++ app/src/main/java/com/goldmantis/shaystudy/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.support.v7.app.AppCompatActivity;
 
 import com.goldmantis.shaystudy.javaBase.MuitThread.Resource;
 import com.goldmantis.shaystudy.javaBase.MuitThread.SemaphoreThreadPool;
